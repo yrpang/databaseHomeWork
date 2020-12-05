991fe48e1526b59889592a85d2fc39627e05710d
yrpang <yrpang@outlook.com>
2020-12-05 22:34:47 +0800

    new framwork

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from homework import create_app
 
-app = create_app({'SHOWINFO': True})
+app = create_app()
 
 if __name__ == "__main__":
+    app = create_app({'SHOWINFO': True})
     app.run(debug=True)
