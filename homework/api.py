49e4299225ff999fa75895fc6e4df723052ec628
yrpang <yrpang@outlook.com>
2020-12-07 16:13:55 +0800

    fix: departmentItem PUT

--- homework/api.py
+++ homework/api.py
@@ -93,31 +93,27 @@ api.add_resource(dormitory, '/dormitory')
 api.add_resource(dormitoryItem, '/dormitory/<string:dormitoryNo>')
 
 
-
-
 # 下面为department的api的实现
 parser_departmentItem = reqparse.RequestParser()
 parser_departmentItem.add_argument('departName', required=True,
-                                  type=str, help="departName not provide.")
+                                   type=str, help="departName not provide.")
 parser_departmentItem.add_argument('departOffice', required=True,
-                                  type=str, help="departOffice not provide.")
+                                   type=str, help="departOffice not provide.")
 parser_departmentItem.add_argument('departNum', required=True,
-                                  type=int, help="departNum not provide.")
+                                   type=int, help="departNum not provide.")
 parser_departmentItem.add_argument('dormitoryNo', required=True,
-                                  type=str, help="dormitoryNo not provide.")
+                                   type=str, help="dormitoryNo not provide.")
 
 
 class departmentItem(Resource):
-    def checkIfExist(self, departNo):            #查询是否存在
+    def checkIfExist(self, departNo):  # 查询是否存在
         cur = get_db().cur
 
-
-        cur.execute("SELECT * FROM Department WHERE departNo='%s'" %departNo)
+        cur.execute("SELECT * FROM Department WHERE departNo='%s'" % departNo)
         if(len(cur.fetchall()) < 1):
             abort(404, message={'errCode': -1, 'status': '操作的系不存在'})
 
-
-    def get(self, departNo):                   
+    def get(self, departNo):
         cur = get_db().cur
 
         cur.execute("SELECT * FROM Department WHERE departNo='%s'" % departNo)
@@ -126,29 +122,24 @@ class departmentItem(Resource):
         if not items:
             return {'errCode': -1, 'status': '请求条目不存在'}
         else:
-            return {'errCode': 0, 'status': 'OK', 'data': {'departNo': items[0], 'departName': items[1], 'departOffice': items[2],'departNum': items[3],'dormitoryNo':items[4]}}
+            return {'errCode': 0, 'status': 'OK', 'data': {'departNo': items[0], 'departName': items[1], 'departOffice': items[2], 'departNum': items[3], 'dormitoryNo': items[4]}}
 
-
-    
-
-    def put(self, departNo):                    #增加
+    def put(self, departNo):  # 增加
         db = get_db()
         cur = get_db().cur
         args = parser_departmentItem.parse_args()
 
         self.checkIfExist(departNo)
 
-
-        #这里有问题
         try:
-            cur.execute("UPDATE Department SET departName='%s',departNo = '%s',departOffice = '%s',departNum = '%d',dormitoryNo = '%s' WHERE departNo='%s';" % (
-                args['departName'], departNo,args['departOffice'],args['departNum'],args['dormitoryNo']))
+            cur.execute("UPDATE Department SET departName='%s',departOffice = '%s',departNum = '%d',dormitoryNo = '%s' WHERE departNo='%s';" % (
+                args['departName'], args['departOffice'], args['departNum'], args['dormitoryNo'], departNo))
             db.commit()
         except Error:
             return {'errCode': -1, 'status': '执行错误'}
         return {'errCode': 0, 'status': 'OK'}, 200
 
-    def delete(self, departNo):              #删除
+    def delete(self, departNo):  # 删除
         self.checkIfExist(departNo)
         db = get_db()
         cur = get_db().cur
@@ -162,12 +153,10 @@ class departmentItem(Resource):
         return {'errCode': 0, 'status': 'OK'}, 200
 
 
-
-#暂时不懂
+# 暂时不懂
 parser_department = parser_departmentItem.copy()
 parser_department.add_argument(
     'departNo', required=True, type=str, help="departNo not provide.")
-
 
 
 class department(Resource):
@@ -175,14 +164,10 @@ class department(Resource):
         cur = get_db().cur
         cur.execute("SELECT * FROM Department;")
 
-    
         res = {'errCode': 0, 'status': 'OK', 'data': [
-            {'departNo': item[0], 'departName': item[1], 'departOffice': item[2],'departNum': item[3],'dormitoryNo':item[4]} for item in cur.fetchall()]}
+            {'departNo': item[0], 'departName': item[1], 'departOffice': item[2], 'departNum': item[3], 'dormitoryNo':item[4]} for item in cur.fetchall()]}
         return res
 
-
-
-    
     def post(self):
         args = parser_department.parse_args()
         db = get_db()
@@ -190,7 +175,7 @@ class department(Resource):
 
         try:
             cur.execute("INSERT INTO Department(departNo, departName,departOffice,departNum,dormitoryNo) VALUES('%s', '%s', '%s', '%d','%s');" % (
-                args['departNo'], args['departName'],args['departOffice'],args['departNum'],args['dormitoryNo']))
+                args['departNo'], args['departName'], args['departOffice'], args['departNum'], args['dormitoryNo']))
             db.commit()
         except Error:
             return {'errCode': -1, 'status': '执行错误'}
