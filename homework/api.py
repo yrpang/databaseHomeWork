015f9c742559159b4eb871117a5a18ca1e870fdf
H.Chen <chenhj2000@163.com>
2020-12-07 20:04:07 +0800

    student

--- homework/api.py
+++ homework/api.py
@@ -102,7 +102,7 @@ parser_departmentItem.add_argument('departOffice', required=True,
 parser_departmentItem.add_argument('departNum', required=True,
                                    type=int, help="departNum not provide.")
 parser_departmentItem.add_argument('dormitoryNo', required=True,
-                                   type=str, help="dormitoryNo not provide.")
+                                   type=str, help="dormitoryNo not provide.")   # TO BE DELETE
 
 
 class departmentItem(Resource):
@@ -184,3 +184,111 @@ class department(Resource):
 
 api.add_resource(department, '/department')
 api.add_resource(departmentItem, '/department/<string:departNo>')
+
+
+
+# 下面为student的api的实现(Hejia Chen)
+parser_studentItem = reqparse.RequestParser()
+parser_studentItem.add_argument('stuNo', required=True, type=str, help="stuNo not provide.")
+parser_studentItem.add_argument('stuName', required=True, type=str, help="stuName not provide.")
+parser_studentItem.add_argument('stuAge', required=True, type=int, help="stuAge not provide.")
+parser_studentItem.add_argument('departNo', required=True, type=str, help="departNo not provide.")
+parser_studentItem.add_argument('classNo', required=True, type=str, help="classNo not provide.")
+parser_studentItem.add_argument('dormitoryNo', required=True, type=str, help="dormitoryNo not provide.")
+
+class studentItem(Resource):
+    def checkIfExist(self, stuNo):  # 查询是否存在
+        cur = get_db().cur
+
+        cur.execute("SELECT * FROM Student WHERE stuNo='%s'" % stuNo)
+        if(len(cur.fetchall()) < 1):
+            abort(404, message={'errCode': -1, 'status': '操作的系不存在'})
+
+    def get(self, stuNo):
+        cur = get_db().cur
+
+        cur.execute("SELECT * FROM Student WHERE stuNo='%s'" % stuNo)
+        items = cur.fetchone()
+        print(items)
+        if not items:
+            return {'errCode': -1, 'status': '请求条目不存在'}
+        else:
+            return {'errCode': 0,
+                    'status': 'OK',
+                    'data': {'stuNo': items[0],
+                             'stuName': items[1],
+                             'stuAge': items[2],
+                             'departNo': items[3],
+                             'classNo': items[4],
+                             'dormitoryNo': items[5]}
+                    }
+
+    def put(self, stuNo):  # 增加
+        db = get_db()
+        cur = get_db().cur
+        args = parser_studentItem.parse_args()
+
+        self.checkIfExist(stuNo)
+        try:
+            cur.execute("UPDATE Student SET stuName='%s',"
+                        "stuAge = '%s',"
+                        "departNo = '%d',"
+                        "classNo = '%s', "
+                        "dormitoryNo='%s'"
+                        "WHERE stuNo='%s';" % (
+                args['stuName'], args['stuAge'], args['departNo'], args['classNo'], args['dormitoryNo'], stuNo))
+            db.commit()
+        except Error:
+            return {'errCode': -1, 'status': '执行错误'}
+        return {'errCode': 0, 'status': 'OK'}, 200
+
+    def delete(self, stuNo):  # 删除
+        self.checkIfExist(stuNo)
+        db = get_db()
+        cur = get_db().cur
+        try:
+            cur.execute(
+                "DELETE FROM Student WHERE stuNo='%s';" % stuNo)
+            db.commit()
+        except Error:
+            return {'errCode': -1, 'status': '执行错误'}
+        return {'errCode': 0, 'status': 'OK'}, 200
+
+parser_student = parser_studentItem.copy()
+parser_student.add_argument('stuNo', required=True, type=str, help="stuNo not provide.")
+
+
+class student(Resource):
+    def get(self):
+        cur = get_db().cur
+        cur.execute("SELECT * FROM Student;")
+
+        res = {'errCode': 0,
+               'status': 'OK',
+               'data': [{'stuNo': item[0],
+                         'stuName': item[1],
+                         'stuAge': item[2],
+                         'departNo': item[3],
+                         'classNo':item[4],
+                         'dormitoryNo': item[5]}
+                        for item in cur.fetchall()]}
+        return res
+
+    def post(self):
+        args = parser_student.parse_args()
+        db = get_db()
+        cur = get_db().cur
+
+        try:
+            cur.execute("INSERT INTO Student(stuNo, stuName,stuAge,departNo, classNo, dormitoryNo) "
+                        "VALUES('%s', '%s', '%s', '%d','%s', '%s');" %
+                        (args['stuNo'], args['stuName'], args['stuAge'],
+                         args['departNo'], args['classNo'], args['dormitoryNo']))
+            db.commit()
+        except Error:
+            return {'errCode': -1, 'status': '执行错误'}
+        return {'errCode': 0, 'status': 'OK'}, 200
+
+
+api.add_resource(student, '/student')
+api.add_resource(departmentItem, '/student/<string:stuNo>')
