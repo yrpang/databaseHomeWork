8f3e61487ca25654a9ed987298a5cd0ae602de30
yrpang <yrpang@outlook.com>
2020-12-21 23:59:31 +0800

    fix: view add: FUNCTION, TRIGGER

--- homework/schema.sql
+++ homework/schema.sql
@@ -27,7 +27,7 @@ create TABLE Student(
   stuName VARCHAR(20) NOT NULL,
   stuAge INT,
   classNo VARCHAR(10) NOT NULL,
-  FOREIGN KEY(classNo) REFERENCES Class(classNo)
+  FOREIGN KEY(classNo) REFERENCES Class(classNo) ON UPDATE CASCADE
 );
 
 CREATE TABLE Association(
@@ -51,6 +51,68 @@ CREATE TABLE Dormitory(
 );
 
 CREATE VIEW NAME_SOCIETY
-  AS
-  SELECT societyName
-	FROM Association;
+AS
+SELECT societyName Name, COUNT(*) Num
+FROM Association
+GROUP BY societyName;
+
+DELIMITER $
+CREATE TRIGGER UPSTUNUM
+AFTER INSERT ON Student
+FOR EACH ROW
+BEGIN
+  UPDATE Class SET classNum=classNum+1 WHERE classNo=NEW.classNo;
+  UPDATE Department SET departNum=departNum+1 WHERE departNo IN (
+    SELECT departNo FROM Class, Student WHERE Class.classNo=NEW.classNo
+  );
+END$
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER DECSTUNUM
+AFTER DELETE ON Student
+FOR EACH ROW
+BEGIN
+  UPDATE Class SET classNum=classNum-1 WHERE classNo=OLD.classNo;
+  UPDATE Department SET departNum=departNum-1 WHERE departNo IN (
+    SELECT departNo FROM Class, Student WHERE Class.classNo=OLD.classNo
+  );
+END$
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER UPDATESTUNUM
+AFTER UPDATE ON Student
+FOR EACH ROW
+BEGIN
+  UPDATE Class SET classNum=classNum+1 WHERE classNo=NEW.classNo;
+  UPDATE Department SET departNum=departNum+1 WHERE departNo IN (
+    SELECT departNo FROM Class, Student WHERE Class.classNo=NEW.classNo
+  );
+  UPDATE Class SET classNum=classNum-1 WHERE classNo=OLD.classNo;
+  UPDATE Department SET departNum=departNum-1 WHERE departNo IN (
+    SELECT departNo FROM Class, Student WHERE Class.classNo=OLD.classNo
+  );
+END$
+DELIMITER ;
+
+
+CREATE FUNCTION change_classNo(old_classNo VARCHAR(20), new_classNo VARCHAR(20))
+RETURNS INT
+BEGIN
+  DECLARE num INT DEFAULT 0;
+
+  UPDATE Class
+  SET classNo=new_classNo
+  WHERE classNo=old_classNo;
+
+  UPDATE Student
+  SET classNo=new_classNo
+  WHERE classNo=old_classNo;
+
+  SELECT COUNT(*) INTO num
+  FROM Student
+  WHERE classNo=new_classNo;
+
+  RETURN(num);
+END
